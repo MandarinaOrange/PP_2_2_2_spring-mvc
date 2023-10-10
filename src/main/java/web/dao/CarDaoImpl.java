146f31c9c35cf4e645d973c3ae231f56aaf610e2
Mandarin <msdolphin@yandex.ru>
2023-10-10 19:23:27 +0300

    adding and trying to open cars

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,11 +1,13 @@
 package web.dao;
 
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Repository
 public class CarDaoImpl implements CarDao{
 
     private static List<Car> carList = new ArrayList<>();
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,29 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CarServiceImpl implements CarService{
+    private static List<Car> cars = new ArrayList<>();
+
+    @Autowired
+    private CarService carService;
+
+
+    static {
+        cars.add(new Car("Toyota", "grey", 432));
+        cars.add(new Car("Mitsubishi", "red", 284));
+        cars.add(new Car("Ferrari", "red", 986));
+        cars.add(new Car("BMW", "blue", 827));
+        cars.add(new Car("Giguli", "cherry", 001));
+    }
     @Override
     public List<Car> getCars(int number) {
-        return null;
+        return carService.getCars(number);
     }
 }
